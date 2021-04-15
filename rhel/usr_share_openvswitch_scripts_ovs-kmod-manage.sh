gvrose8192/ovs-temp

commit 116a5cfe9a3fdf5e2c3e8c77e65d51ab3fc041ea

rhel: Do not update kmod RPM newer major revision kernels.
The ovs-kmod-manage.sh script will run weak-updates even on newer
release kernels installing a non-compatible or un-runnable kernel
module.

Update the script to never install weak-updates onto kernels with
newer major release versions.

VMware-BZ: #2717283
Signed-off-by: Greg Rose <gvrose8192@gmail.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

rhel/usr_share_openvswitch_scripts_ovs-kmod-manage.sh
@@ -155,6 +155,16 @@ kmod_versions=()
 kversion=$(rpm -ql ${rpmname} | grep '\.ko$' | \
            sed -n -e 's/^\/lib\/modules\/\(.*\)\/extra\/.*$/\1/p' | \
            sort | uniq)
+
+IFS='.\|-' read installed_major installed_minor installed_patch \
+    installed_major_rev installed_minor_rev installed_extra <<<"${kversion}"
+
+if [ "$installed_major_rev" -lt "$major_rev" ]; then
+    echo "Not installing RPM with major revision $installed_major_rev" \
+         "to kernel with greater major revision $major_rev.  Exiting"
+    exit 1
+fi
+
 for kv in $kversion; do
     IFS='.\|-' read -r -a kv_nums <<<"${kv}"
     kmod_versions+=(${kv_nums[$ver_offset]})

commit a2d4a218f7df67dd9713324c7b4d219d123432a3

rhel: Also check major revision
Fixes: 116a5cfe9a3f ("rhel: Do not update kmod RPM newer major revision
kernels")

The check for RPM newer major revision should ignore the SLES kernels,
it is not meant for them.

Passes a Jarvis pre-checkin test here:
https://racetrack.eng.vmware.com/resultdetails_nsx.php?id=545662095&resultid=13069618

VMware-BZ: #2751264
Signed-off-by: Greg Rose <roseg@vmware.com>

rhel/usr_share_openvswitch_scripts_ovs-kmod-manage.sh
@@ -159,7 +159,7 @@ kversion=$(rpm -ql ${rpmname} | grep '\.ko$' | \
 IFS='.\|-' read installed_major installed_minor installed_patch \
     installed_major_rev installed_minor_rev installed_extra <<<"${kversion}"
 
-if [ "$installed_major_rev" -lt "$major_rev" ]; then
+if [ "$installed_major_rev" -lt "$major_rev" ] && [ "$mainline_major" = "3" ]; then
     echo "Not installing RPM with major revision $installed_major_rev" \
          "to kernel with greater major revision $major_rev.  Exiting"
     exit 1